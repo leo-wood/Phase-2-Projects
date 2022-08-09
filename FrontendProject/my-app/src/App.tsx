338fb6df093555d8d43bcba428ff8cba7a18facc
Leo <leorwood@gmail.com>
2022-08-09 19:56:08 +1200

    Added a list functionality
    --can only pop from the list
    -- must do another action before the pop actually happens

--- FrontendProject/my-app/src/App.tsx
+++ FrontendProject/my-app/src/App.tsx
@@ -1,20 +1,39 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import axios from "axios";
-import logo from './logo.svg';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import RefreshIcon from '@mui/icons-material/Refresh';
 import './App.css';
+import { title } from 'process';
+
 
 function App() {
 
   const [movieName, setMovieName] = useState("");
   const [movieInfo, setMovieInfo] = useState<undefined | any>(undefined);
 
+  const [movieList, setMovieList] = useState([
+    {
+      Title: "Default movie",
+      Year: "1998"
+    }
+  ]);
+
   const MOVIE_BASE_URL = "http://www.omdbapi.com/?t=";
   const APIKEY = "apikey=7cca2981";
-  
+
   return (
     <div>
         <h1>
-            Search for your favourite movie to check off your list
+            Search for information on any movie using the OMDB API
         </h1>
 
         <div>
@@ -36,12 +55,24 @@ function App() {
               <div>{movieInfo.Director}</div>
               <div>{movieInfo.Runtime}</div>
               <div>{movieInfo.Genre}</div>
-              
-              
             </p>
           )}
-        
+
+          <div>
+            <ul>
+              {movieList.map((movie) => (
+                <div key={movie.Title}>
+                  <p>{movie.Title}, {movie.Year}</p>
+                  <hr/>
+                </div>
+              ))}
+            </ul>
+            <button onClick={handleAddNewMovie}>Add movie</button>
+            <button onClick={handleRemoveMovie}>Pop the latest</button>
+            </div>    
     </div>
+
+    
   );
 
   function search(){
@@ -50,6 +81,29 @@ function App() {
       setMovieInfo(res.data);
 
     })
+  }
+  
+  function handleAddNewMovie(){ 
+    console.log(movieList.indexOf(movieInfo.Title))
+    const updateMovies = [
+        ...movieList, {
+          Title: movieInfo.Title,
+          Year: movieInfo.Year
+        }
+      ];
+      setMovieList(updateMovies)
+
+  }
+
+  function handleRemoveMovie(){
+    movieList.pop();
+    const updateMovies = [
+      ...movieList, {
+        Title: movieInfo.Title,
+        Year: movieInfo.Year
+      }
+    ];
+    setMovieList(movieList);
   }
 
 }
