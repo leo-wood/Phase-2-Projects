2345a51ebb085fdd69e30e41c11ed840c5d72f68
Leo <leorwood@gmail.com>
2022-08-10 18:45:00 +1200

    Updated README
    Added configuration demonstration in README

--- BackendProject/README.md
+++ BackendProject/README.md
@@ -1,8 +1,20 @@
 # My Backend Project for MSA Phase 2
 This project utilises the PokeAPI to retrieve a pokemon via their name and then adds them to your team.
 
+Below are the demonstration requirements 
+
+### Difference in Config files
+
+The development configuration file defines the logging level as "Information", the default level. 
+Allowed hosts is set to "*" allowing for requests to be made by any port number
+The production configuration file allows for a connection to be made to a potential database. This is not currently used.
 
 
-### Demonstrate --
+### Demonstrate how these middleware via DI (dependency injection) simplifies your code.
 
-### Demonstrate --
+
+
+### Demonstrate why the middleware libraries made your code easier to test.
+
+NUnit allows for quicker and easier testing within the project. Using the NUnit middleware framework, the [Test] attribute lets us mark a class as a test and then using NUnit's built-in Assert class to test whether my function is working as expected. This is demonstrated in my PokemonIsCreated() Test class.
+
--- BackendProject/WepApiProject/TestPokeApi/UnitTest1.cs
+++ BackendProject/WepApiProject/TestPokeApi/UnitTest1.cs
@@ -39,7 +39,7 @@ namespace TestPokeApi
         /// its recommended for Interfaces which my project doesnt have. 
         /// So i've tried to make some use of it but its not very effective.
         /// --------------
-        /// Tests pokemon with different values are not equal
+        /// Tests pokemon with different values are not equal using a substitute
         /// </summary>
         [Test]
         public void Pokemon_are_different()
@@ -57,13 +57,16 @@ namespace TestPokeApi
             substitutePokemon.name = "SubtestPokemon";
             substitutePokemon.base_experience = 20;
 
-            Assert.That(pokemon, Is.Not.EqualTo(substitutePokemon));
-            
+            //their names being equal shows the pokemon are correctly accessed for the below test
             Assert.That(pokemon.name, Is.EqualTo(substitutePokemon.name));
 
-
-
+            //pokemon should not == substitute as they have different base exp
+            Assert.That(pokemon, Is.Not.EqualTo(substitutePokemon));
+            
+            
+            
 
         }
+
     }
 }
